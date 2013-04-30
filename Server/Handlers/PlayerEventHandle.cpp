f601620c70ebd2144f7ea56aff1d55b44dc9c5b7
Anton <zx.anton95@gmail.com>
2013-04-30 16:56:30 +0800

    Complete UpdateEventHandle And PlayerEventHandle
    20130430

--- Server/Handlers/PlayerEventHandle.cpp
+++ Server/Handlers/PlayerEventHandle.cpp
@@ -3,4 +3,63 @@
 void Handler::PlayerEventHandle(const PlayerEvent &event,const QHostAddress &address)
 {
 	qDebug()<<"Get PlayerEvent";
+	QSqlQuery query;
+	bool flag;
+	query.prepare("SELECT Type FROM Cube WHERE Position=? AND Type=?");
+	query.addBindValue(Utils::toInt(event.getPosition()));
+	query.addBindValue(1); //To do
+	flag=query.exec();
+
+	if(flag) qDebug()<<"Query Cube In PlayerEventHandle Successed";
+	else qDebug()<<"Query Cube In PlayerEventHandle Failed";
+	if(query.first()){
+		qDebug()<<"Can Move To This Position";
+		query.prepare("UPDATE Player SET Position=? WHERE PName=?");
+		query.addBindValue(Utils::toInt(event.getPosition()));
+		query.addBindValue(userMap[address]);
+		flag=query.exec();
+
+		if(flag) qDebug()<<"Update Player In PlayerEventHandle Successed";
+		else qDebug()<<"Update Player In PlayerEventHandle Failed";
+	}
+	else qDebug()<<"Can Not Move To This Position";
+
+	foreach(auto it,event.getPackage()){
+		query.prepare("SELECT Number FROM Cell WHERE PName=? AND Item=?");
+		query.addBindValue(userMap[address]);
+		query.addBindValue(it.first);
+		flag=query.exec();
+
+		if(flag) qDebug()<<"Query Cell For Item "<<it.first<<"In PlayerEventHandle Successed";
+		else qDebug()<<"Query Cell For Item "<<it.first<<"In PlayerEventHandle Failed";
+		if(query.first()){
+			qDebug()<<"Find Item "<<it.first<<" In The Cell Then Update Cell";
+			int number=query.record().value("Number").toInt();
+			if(number+it.second>=0){
+				qDebug()<<"Update Valid";
+				query.prepare("UPDATE Cell SET Number=? WHERE WHERE PName=? AND Item=?");
+				query.addBindValue(number+it.second);
+				query.addBindValue(userMap[address]);
+				query.addBindValue(it.first);
+				flag=query.exec();
+				if(flag) qDebug()<<"Update Cell For Item "<<it.first<<"In PlayerEventHandle Successed";
+				else qDebug()<<"Update Cell For Item "<<it.first<<"In PlayerEventHandle Failed";
+			}
+			else qDebug()<<"Update Invalid";
+		}
+		else{
+			qDebug()<<"Do Not Find Item "<<it.first<<" In The Cell The Insert Into Cell";
+			if(it.second>=0){
+				qDebug()<<"Insert Valid";
+				query.prepare("INSERT INTO Cell (PName,Item,Number)""VALUES(?,?,?)");
+				query.addBindValue(userMap[address]);
+				query.addBindValue(it.first);
+				query.addBindValue(it.second);
+				flag=query.exec();
+				if(flag) qDebug()<<"Insert Into Cell For Item "<<it.first<<"In PlayerEventHandle Successed";
+				else qDebug()<<"Insert Into Cell For Item "<<it.first<<"In PlayerEventHandle Failed";
+			}
+			else qDebug()<<"Insert Invalid";
+		}
+	}
 }
--- Server/Handlers/UpdateEventHandle.cpp
+++ Server/Handlers/UpdateEventHandle.cpp
@@ -5,10 +5,29 @@ void Handler::UpdateEventHandle(const UpdateEvent &event,const QHostAddress &add
 	qDebug()<<"Get UpdateEvent";
 	const QRect rect=event.getRect();
 	QVector<BitType> bitmap;
+	QSqlQuery query;
+	bool flag;
 	int x=rect.x(),y=rect.y(),w=rect.width(),h=rect.height();
-	for(int i=0;i<h;i++){
-		for(int j=0;j<w;j++){
-			bitmap.append((((x+j)/2+(y+i)/2)&1)+3);
+	QVector<BitType> temp[w+1];
+	for(int j=x;j<=x+w;j++){
+		query.prepare("SELECT Type FROM Cube WHERE Position>=? AND Position<=?");
+		query.addBindValue(j<<16+y);
+		query.addBindValue(j<<16+y+h);
+		flag=query.exec();
+		if(flag) qDebug()<<"Query Cube In The UpdateEventHandle Successed";
+		else qDebug()<<"Query Cube In The UpdateEventHandle Failed";
+		if(query.first()){
+			qDebug()<<"Start To Update Bitmap From Point "<<j<<","<<y<<" In The UpdateEventHandle";
+			while(query.next()){
+				int type=query.value("Type").toInt();
+				temp[j-x].append(type);
+			}
+		}
+		else qDebug()<<"Update Bitmap From Point "<<j<<","<<y<<"In The UpdateEventHandle Failed";
+	}
+	for(int i=0;i<=h;i++){
+		for(int j=0;j<=w;j++){
+			bitmap.append(temp[j].at(i));
 		}
 	}
 	UpdateEvent reply;
